93a09a8b7d402921eab67eba11f87664b74c90d8
nahidnstu12 <nahid.dev19@gmail.com>
2022-10-05 23:52:37 +0600

    model added

---
+++ src/api/image-gallary/routes/image-gallary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-gallary router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::image-gallary.image-gallary');
---
+++ src/api/image-gallary/services/image-gallary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-gallary service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::image-gallary.image-gallary');
